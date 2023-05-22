eca5701ff922504d09da38e5a9ab0c830f6bcfc9
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-05-22 14:31:44 +0900

    05_22_commit

--- src/components/Maincomponents/index.js
+++ src/components/Maincomponents/index.js
@@ -1,11 +1,13 @@
 import {useState, useEffect} from 'react';
 import {Box, Center, SimpleGrid, Flex, Text} from '@chakra-ui/react'
 import {getData} from "../../api/getData"
-import { useQuery } from '@tanstack/react-query';
+import {useQuery} from '@tanstack/react-query';
 function MainComponent() {
     //const [data, setData] = useState([])
-    const {data,isLoading, error} = useQuery(["wear"],()=>getData(), {
-        initialData: { data: [] },
+    const {data, isLoading, error} = useQuery(["wear"], () => getData(), {
+        initialData: {
+            data: []
+        }
     });
     console.log(data.data)
 
@@ -15,30 +17,38 @@ function MainComponent() {
         })
 
     }, [])*/
-    return (<> < Center > <Flex w={"40vh"} h={"100vh"} flexDir={"column"}>
-        <Text margin={"0 auto"} lineHeight={"10vh"} fontSize={"2xl"}>무신사 랭킹</Text>
-        <SimpleGrid bg={"#eee"} columns={1} spacingY={5} padding={5}>
-            { isLoading ? null:
-                data.data.map((item, index) => {
-                    return (
-                        <Box height='130px' border="1px">
-                            <Text>{item[0]}등</Text>
-                            <Text>{item[3]}</Text>
-                            <Flex
-                                marginTop={3}
-                                height={"60px"}
-                                flexDir={"row"}
-                                justifyContent={"space-between"}>
-                                <Text>{item[4]}</Text>
-                                <Text w={"70px"} textAlign={"center"}>{item[7]}</Text>
-                            </Flex>
-                        </Box>
-                    )
-                })
-            }
-        </SimpleGrid>
-    </Flex>
-</Center> </>
+    return (
+        <> < Center > <Flex w={"40vh"} h={"100vh"} flexDir={"column"}>
+            <Text margin={"0 auto"} lineHeight={"10vh"} fontSize={"2xl"}>무신사 랭킹</Text>
+            <SimpleGrid columns={1} spacingY={5} padding={5}>
+                {
+                    isLoading
+                        ? null
+                        : data
+                            .data
+                            .map((item, index) => {
+                                return (
+                                    <Box height='100px' border="0.5px solid #e4e4e4" padding={3} shadow={"md"}>
+                                        <Flex
+                                            marginTop={3}
+                                            height={"60px"}
+                                            flexDir={"column"}
+                                            justifyContent={"space-between"}>
+                                            <Text
+                                                w={"100%"}
+                                                fontWeight={"bold"}
+                                                whiteSpace={"nowrap"}
+                                                overflow={"hidden"}
+                                                textOverflow={"ellipsis"}>
+                                                {item[3]}</Text>
+                                            <Text>{item[4]}</Text>
+                                        </Flex>
+                                    </Box>
+                                )
+                            })
+                }
+            </SimpleGrid>
+        </Flex> < /Center> </ >
     );
 }
 
